086eef18d4bf9a1b494aa559f0cfb656644ddc7a
Marcos Daniel Torres <marcoss.nqn@gmail.com>
2024-06-29 15:27:14 -0300

    who cares

--- aim-v5/engine/assets/1.colors.fs
+++ aim-v5/engine/assets/1.colors.fs
@@ -4,7 +4,10 @@ out vec4 FragColor;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 
-void main()
-{
-    FragColor = vec4(lightColor * objectColor, 1.0);
+void main() {
+    float ambient_strength = 0.1;
+    vec3 ambient = ambient_strength * lightColor;
+
+    vec3 result = ambient * objectColor;
+    FragColor = vec4(result, 1.0); // set all 4 vector values to 1.0
 }
--- aim-v5/engine/src/entry.h
+++ aim-v5/engine/src/entry.h
@@ -13,6 +13,12 @@
 // este SI anda
 #include "core/logger/logger.h"
 
+// imgui includes
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+
 /*
 TODO:
 	- Renderizar un triangulo al menos
@@ -51,6 +57,23 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 
+static bool gui_mode = false;
+
+struct Transform3D {
+	glm::vec3 pos;
+	glm::vec3 scale;
+};
+
+struct Light {
+	Transform3D light_transform;
+	glm::vec3 color;
+};
+
+struct Model {
+	Transform3D model_transform;
+	glm::vec3 unlit_color;
+};
+
 
 // settings
 const unsigned int SCR_WIDTH = 1700;
@@ -67,7 +90,12 @@ float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 // lighting
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 light_pos(1.2f, 1.0f, 2.0f);
+glm::vec3 light_scale(0.2f);
+
+// model
+glm::vec3 model_pos(0.0f, 0.0f, 0.0f);
+glm::vec3 model_scale(1.0f);
 
 
 
@@ -170,7 +198,7 @@ int main() {
 	create_game(&game_inst);
 
 	game_inst.init(&game_inst);
-	
+
 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -203,6 +231,19 @@ int main() {
 	}
 	INFO("OpenGL initialized successfully!");
 
+#pragma region imgui
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+	ImGui_ImplOpenGL3_Init();
+#pragma endregion imgui
 
 
 
@@ -287,27 +328,26 @@ int main() {
 
 	// render loop
 	// -----------
+	glm::vec3 light_color = glm::vec3(1.0f, 1.0f, 1.0f);
+	glm::vec3 model_color =	glm::vec3(1.0f, 0.5f, 0.31f);
+
 	while (!glfwWindowShouldClose(window))
 	{
-		// per-frame time logic
-		// --------------------
 		float currentFrame = static_cast<float>(glfwGetTime());
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		// input
-		// -----
 		processInput(window);
 
-		// render
-		// ------
+
+#pragma region render
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// be sure to activate shader when setting uniforms/drawing objects
 		lightingShader.use();
-		lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lightingShader.setVec3("objectColor",model_color.r, model_color.g, model_color.b);
+		lightingShader.setVec3("lightColor", light_color.r, light_color.g, light_color.b);
 
 		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -317,6 +357,8 @@ int main() {
 
 		// world transformation
 		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, model_pos);
+		model = glm::scale(model, model_scale); // a smaller cube
 		lightingShader.setMat4("model", model);
 
 		// render the cube
@@ -328,34 +370,191 @@ int main() {
 		lightCubeShader.use();
 		lightCubeShader.setMat4("projection", projection);
 		lightCubeShader.setMat4("view", view);
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, lightPos);
-		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-		lightCubeShader.setMat4("model", model);
+		glm::mat4 light_model = glm::mat4(1.0f);
+		light_model = glm::translate(light_model, light_pos);
+		light_model = glm::scale(light_model, light_scale); // a smaller cube
+		lightCubeShader.setMat4("model", light_model);
+
+		lightCubeShader.setVec3("lightColor", light_color.r, light_color.g, light_color.b);
 
 		glBindVertexArray(lightCubeVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
-		glfwSwapBuffers(window);
+		/* TODO IMGUI:
+		 LA CONCHA DE TU MADRE
+
+		- Inicializar imgui con OpenGL should be fucking straightforward (jua)
+		- I want show:
+
+			proj_matrix
+			view_matrix
+
+			light transform:
+			light_pos
+			light_scale
+			light_rot
+			light_matrix
+
+			light_color
+
+
+			model transform:
+			model_pos
+			model_scale
+			model_rot
+			model_matrix
+
+			model_unlit_color
+			model_lit_color
+
+			cam_pos
+		*/
+
 		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// Demo Window useful for inspecting its implementation
+		//ImGui::ShowDemoWindow();
+
+
+
+
+		// Start the parent window
+		//ImGui::Begin("Parent Window");
+
+		//// Number of objects or child windows
+		//int numberOfObjects = 5;
+
+		// Iterate through each object and create a child window for it
+		//for (int i = 0; i < numberOfObjects; i++) {
+		//    // Define a unique ID for each child based on the object index
+		//    std::string child_id = "Object " + std::to_string(i);
+
+		//    // Start the child window
+		//    if (ImGui::BeginChild(child_id.c_str(), ImVec2(0, 100), true)) {
+		//        ImGui::Text("Content of Object %d", i);
+
+		//		ImGui::ColorPicker3("Something", reinterpret_cast<float*>(&light_color));
+		//		ImGui::CollapsingHeader("Expanded Header", ImGuiTreeNodeFlags_DefaultOpen);
+		//		ImGui::Text("This header is expanded by default.");
+		//        // Add more UI elements related to the object here
+		//        // For example, sliders, buttons, etc.
+		//    }
+		//    ImGui::EndChild(); // End the child window
+
+		//    // Optionally, add some space between child windows
+		//    ImGui::Spacing();
+		//}
+		//ImGui::End(); // End the parent window
+
+			//static ImVec4 col{};
+
+		ImGuiWindowFlags global_flags = ImGuiWindowFlags_None;
+		if (!gui_mode) {
+			global_flags = ImGuiWindowFlags_NoInputs;
+		}
+
+
+		ImGui::Begin("Model", nullptr, global_flags);
+		if (ImGui::CollapsingHeader("model transform", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGui::DragFloat3("model pos", glm::value_ptr(model_pos), 0.1f);
+			ImGui::DragFloat3("model scale", glm::value_ptr(model_scale), 0.1f);
+			// TODO: Add rotation as well...
+		}
+
+		if (ImGui::CollapsingHeader("model material", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGuiColorEditFlags flags = ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_Float;
+			ImGui::ColorEdit3("model color", reinterpret_cast<float*>(&model_color), flags);
+		}
+		ImGui::End();
+
+
+		ImGui::Begin("light", nullptr, global_flags);
+		if (ImGui::CollapsingHeader("light transform", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGui::DragFloat3("light pos", glm::value_ptr(light_pos), 0.1f);
+			ImGui::DragFloat3("light scale", glm::value_ptr(light_scale), 0.1f);
+			// TODO: Add rotation as well...
+		}
+
+		if (ImGui::CollapsingHeader("light model matrix", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGui::InputFloat4("", glm::value_ptr(light_model[0]));
+			ImGui::InputFloat4("", glm::value_ptr(light_model[1]));
+			ImGui::InputFloat4("", glm::value_ptr(light_model[2]));
+			ImGui::InputFloat4("", glm::value_ptr(light_model[3]));
+		}
+
+		if (ImGui::CollapsingHeader("light material", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGuiColorEditFlags flags = ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_Float;
+			ImGui::ColorEdit3("light color", reinterpret_cast<float*>(&light_color), flags);
+		}
+		ImGui::End();
+
+
+		ImGui::Begin("camera", nullptr, global_flags);
+
+		if (ImGui::CollapsingHeader("camera transform", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGui::DragFloat3("cam pos", glm::value_ptr(camera.Position), 0.1f);
+			ImGui::DragFloat3("cam forward", glm::value_ptr(camera.Front), 0.1f);
+			ImGui::DragFloat3("cam up", glm::value_ptr(camera.Up), 0.1f);
+			ImGui::DragFloat3("cam right", glm::value_ptr(camera.Right), 0.1f);
+			ImGui::DragFloat3("cam world up", glm::value_ptr(camera.WorldUp), 0.1f);
+			ImGui::Spacing();
+			ImGui::DragFloat("cam yaw", &camera.Yaw, 0.1f);
+			ImGui::DragFloat("cam pitch", &camera.Pitch, 0.1f);
+			// TODO: Add rotation as well...
+		}
+
+
+		if (ImGui::CollapsingHeader("proj matrix", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGui::InputFloat4("", glm::value_ptr(projection[0]));
+			ImGui::InputFloat4("", glm::value_ptr(projection[1]));
+			ImGui::InputFloat4("", glm::value_ptr(projection[2]));
+			ImGui::InputFloat4("", glm::value_ptr(projection[3]));
+		}
+
+		if (ImGui::CollapsingHeader("cam view matrix", ImGuiTreeNodeFlags_DefaultOpen)) {
+			ImGui::InputFloat4("", glm::value_ptr(view[0]));
+			ImGui::InputFloat4("", glm::value_ptr(view[1]));
+			ImGui::InputFloat4("", glm::value_ptr(view[2]));
+			ImGui::InputFloat4("", glm::value_ptr(view[3]));
+		}
+
+		ImGui::End();
+
+
+		ImGui::Begin("perf", nullptr, global_flags);
+		float converted_delta_time = deltaTime * 1000.0f;
+		ImGui::InputFloat("delta time (in ms)", &converted_delta_time);
+		ImGui::End();
+
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+#pragma endregion render
+
+		glfwSwapBuffers(window);
 	}
 
-	// optional: de-allocate all resources once they've outlived their purpose:
-	// ------------------------------------------------------------------------
 	glDeleteVertexArrays(1, &cubeVAO);
 	glDeleteVertexArrays(1, &lightCubeVAO);
 	glDeleteBuffers(1, &VBO);
 
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
 	glfwTerminate();
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 	return 0;
-
-
 }
+
+// weird behaviour with chars int uints int8 etc
+unsigned int bitflag_base = 0x00034000;
+unsigned int bitflag = 0x03;
 
 
 void processInput(GLFWwindow* window)
@@ -363,15 +562,56 @@ void processInput(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
+	if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
 
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, deltaTime);
+		bitflag = bitflag ^ 07;
+		uint32_t value = bitflag_base | bitflag;
+		glfwSetInputMode(window, GLFW_CURSOR, value);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS) {
+		gui_mode = true;
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_CAPTURED);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS) {
+		gui_mode = false;
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	}
+
+	/*
+	* TODO:
+	* When I press G it will switch between GUI mode and game mode.
+	* - In GUI mode the camera will NOT move and the cursor will be captured
+	* - In game mode the camera will move and the cursor will NOT be captured
+	Basically switch between DISABLED and CAPTURED
+
+	03 o 04 xoreados con 07 y listo
+
+	int bitflag_base = 0x00034000
+
+	current_bitflag = current_bitflag ^ 07
+
+	// maybe the shifting is not needed if i use a in32. Should try...
+	// uint32_t or int32_t?
+
+	current_bitflag =  (bitflag_base << 4)  | current_bitflag
+
+	*/
+	//#define GLFW_CURSOR_DISABLED        0x00034003
+	//#define GLFW_CURSOR_CAPTURED        0x00034004
+
+
+	if (!gui_mode) {
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+			camera.ProcessKeyboard(FORWARD, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+			camera.ProcessKeyboard(BACKWARD, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+			camera.ProcessKeyboard(LEFT, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+			camera.ProcessKeyboard(RIGHT, deltaTime);
+	}
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -402,7 +642,10 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 	lastX = xpos;
 	lastY = ypos;
 
-	camera.ProcessMouseMovement(xoffset, yoffset);
+	
+	if (!gui_mode) {
+		camera.ProcessMouseMovement(xoffset, yoffset);
+	}
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
